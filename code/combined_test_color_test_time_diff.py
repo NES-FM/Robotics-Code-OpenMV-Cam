8fb99619672f5b6a5b8affae98bea9fc22d958db
NES-FM <flori.sonstwas@outlook.de>
2022-12-04 20:26:49 +0100

    Reformatting

--- code/combined_test_color_test_time_diff.py
+++ code/combined_test_color_test_time_diff.py
@@ -1,83 +1,85 @@
-import sensor, image, time, os, tf, math, uos, gc, pyb, omv, gc
+import sensor, time, tf, math, gc, pyb, omv
+import uos # type: ignore
+from image import Image
+from lib import lib_ard
 
-gc.enable()
+### CONFIG
+OMV_DEBUG = True # Whether to show framebuffer in OpenMV IDE or not
+min_confidence = 0.4 # Min Config for TensorFlow Detection
+circle_thresh = 1800 # Detection Threshold for circle detection
+### ENDCONFIG
 
-OMV_DEBUG = True
-omv.disable_fb(True)
-
-#omv.disable_fb(True)
-
-#pyb.LED(4).on()
-
-sensor.reset()                         # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
-sensor.set_windowing((240, 240))        # Set 240x240 window.
-#sensor.skip_frames(time=2000)          # Let the camera adjust.
-
-net = None
-labels = None
-min_confidence = 0.4
-
+### GLOBAL VARIABLES
+# default IDs
 background_id = 0
 exit_line_id = 10
 silver_ball_id = 11
 black_ball_id = 12
 corner_id = 13
 
-try:
-    # load the model, alloc the model file on the heap if we have at least 64K free after loading
-    net = tf.load("trained.tflite", load_to_fb=uos.stat('trained.tflite')[6] > (gc.mem_free() - (64*1024)))
-except Exception as e:
-    raise Exception('Failed to load "trained.tflite"(' + str(e) + ')')
-
-try:
-    labels = [line.rstrip('\n') for line in open("labels.txt")]
-
-    for i, label in enumerate(labels):
-        if label == "background":
-            background_id = i
-        elif label == "black_ball":
-            black_ball_id = i
-        elif label == "silver_ball":
-            silver_ball_id = i
-        elif label == "corner":
-            corner_id = i
-    print(f"IDs: backg={background_id} silver={silver_ball_id} black={black_ball_id} corner={corner_id}")
-except Exception as e:
-    raise Exception('Failed to load "labels.txt"(' + str(e) + ')')
-
-colors = [(0, 0, 0)] * 15
-colors[black_ball_id]  = (255, 0,   0  )
-colors[exit_line_id]   = (0,   0,   255)
-colors[silver_ball_id] = (0,   255, 0  )
-colors[corner_id]      = (255, 255, 0  )
-
-circle_thresh = 1800
-
-grayscale_thres = (0, 136)
-
-w = sensor.width()
-h = sensor.height()
-
-TARGET_POINTS = [(0,   0),   # (x, y) CHANGE ME!
-                 (w-1, 0),   # (x, y) CHANGE ME!
-                 (w+47, h-1), # (x, y) CHANGE ME!
-                 (0-48,   h-1)] # (x, y) CHANGE ME!
-
-clock = time.clock()
+colors = []
 
 black_white = True
+net = None
+labels = None
 
 balls: dict
 corner: dict
 exit_line: dict
 img: Image
+### ENDGLOBAL
+
+def init_sensor():
+    omv.disable_fb(True)
+    
+    sensor.reset()                         # Reset and initialize the sensor.
+    sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
+    sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
+    sensor.set_windowing((240, 240))        # Set 240x240 window.
+    sensor.skip_frames(time=500)          # Let the camera adjust.
+
+def load_tf_models():
+    global net, labels, background_id, exit_line_id, silver_ball_id, black_ball_id, corner_id
+    try:
+        # load the model, alloc the model file on the heap if we have at least 64K free after loading
+        net = tf.load("trained.tflite", load_to_fb=uos.stat('trained.tflite')[6] > (gc.mem_free() - (64*1024)))  # type: ignore
+    except Exception as e:
+        raise Exception('Failed to load "trained.tflite"(' + str(e) + ')')
+
+    try:
+        labels = [line.rstrip('\n') for line in open("labels.txt")]
+
+        for i, label in enumerate(labels):
+            if label == "background":
+                background_id = i
+            elif label == "black_ball":
+                black_ball_id = i
+            elif label == "silver_ball":
+                silver_ball_id = i
+            elif label == "corner":
+                corner_id = i
+        print(f"IDs: backg={background_id} silver={silver_ball_id} black={black_ball_id} corner={corner_id}")
+    except Exception as e:
+        raise Exception('Failed to load "labels.txt"(' + str(e) + ')')
+
+def init_colors():
+    global colors
+    for i in range(0, 15):
+        if i == black_ball_id:
+            colors.append((255, 0,   0  ))
+        elif i == exit_line_id:
+            colors.append((0,   0,   255))
+        elif i == silver_ball_id:
+            colors.append((0,   255, 0  ))
+        elif i == corner_id:
+            colors.append((255, 255, 0  ))
+        else:
+            colors.append((0, 0, 0))
 
 def black_white_handler():
     global balls, corner, img
-    balls = {}
-    corner = {}
+    balls.clear()
+    corner.clear()
 
     # Tensorflow detection
     for i, detection_list in enumerate(net.detect(img, thresholds=[(math.ceil(min_confidence * 255), 255)])):
@@ -86,16 +88,18 @@ def black_white_handler():
 
         for d in detection_list:
             if i == black_ball_id:  # black ball
-                balls[d.rect()] = {"classified_as": "black", "value": d.output(), "circles": [], "histogram": img.get_histogram(roi=d.rect())}
+                balls[d.rect()] = {"classified_as": "black", "value": d.output(), "circles": [], "histogram": img.get_histogram(roi=d.rect()), "conf": -1, "histo_class": "unknown"}
             elif i == corner_id:  # corner
                 corner[d.rect()] = {"classified_as": "corner", "value": d.output(), "circles": [], "histogram": img.get_histogram(roi=d.rect()), "blob": None}
             elif i == silver_ball_id:  # silver_ball
-                balls[d.rect()] = {"classified_as": "silver", "value": d.output(), "circles": [], "histogram": img.get_histogram(roi=d.rect())}
+                balls[d.rect()] = {"classified_as": "silver", "value": d.output(), "circles": [], "histogram": img.get_histogram(roi=d.rect()), "conf": -1, "histo_class": "unknown"}
+                
     # For every ball found, search for circles within the area
     for (x, y, w, h), data in balls.copy().items():
         sx, sy, sw, sh = x-16, y-16, w+32, h+32
         for c in img.find_circles(roi = (sx, sy, sw, sh), threshold = circle_thresh, x_margin = int(sw/3), y_margin = int(sh/3), r_margin = 100, r_min = 5, r_max = min(sw, sh), r_step = 1):
             balls[(x, y, w, h)]["circles"].append(c)
+            
     # Search for black blobs as corners:
     for b in img.find_blobs([(0, 17)], merge=True, margin=5):
         (x, y, w, h) = b.rect()
@@ -104,6 +108,10 @@ def black_white_handler():
             if conf > 0.8:
                 corner[b.rect()] = {"classified_as": "corner", "value": 1, "circles": [], "histogram": img.get_histogram(roi=b.rect()), "blob": b, "conf": conf}
                 print(f"Corner:Blob x{x} y{y} w{w} h{h} conf{conf} pix{b.pixels()} rot{b.rotation_deg()}° round{b.roundness()} elong{b.elongation()} dens{b.density()} convex{b.convexity()}")
+    # Only keep highest confidence corner
+    for key, _ in sorted(corner.items(), key=lambda x:x[1]["conf"])[:-1]:
+        corner.pop(key)
+                
     # Additional Informations + result for each ball
     for rec, data in balls.copy().items():
         v = data["value"]
@@ -132,13 +140,16 @@ def black_white_handler():
 
 def rgb_handler():
     global exit_line, img
-    exit_line = {}
+    exit_line.clear()
 
     # Search for green blobs as Exit Lines:
     for b in img.find_blobs([(16, 28, -17, -7, 0, 13)], merge=True, margin=2):
         (x, y, w, h) = b.rect()
-        exit_line[b.rect()] = {"classified_as": "corner", "value": 1, "circles": [], "histogram": img.get_histogram(roi=b.rect()), "blob": b}
+        exit_line[b.rect()] = {"classified_as": "exit", "value": 1, "circles": [], "histogram": img.get_histogram(roi=b.rect()), "blob": b, "conf": 1}
         print(f"Exit: x{x} y{y} w{w} h{h} pix{b.pixels()} rot{b.rotation_deg()}° round{b.roundness()} elong{b.elongation()} dens{b.density()} convex{b.convexity()}")
+    # Only keep highest confidence line
+    for key, _ in sorted(exit_line.items(), key=lambda x:x[1]["conf"])[:-1]:
+        exit_line.pop(key)
 
 def drawing_handler():
     global balls, corner, exit_line, img
@@ -161,22 +172,31 @@ def drawing_handler():
     for (x, y, w, h), data in exit_line.items():
         img.draw_rectangle(x, y, w, h, colors[exit_line_id], 1, False)
 
-while(True):
-    clock.tick()
+if __name__ == '__main__':
+    clock = time.clock()  # type: ignore
+    gc.enable()
 
-    if black_white:
-        sensor.set_pixformat(sensor.GRAYSCALE)
-        img = sensor.snapshot()
-        black_white_handler()
-        if OMV_DEBUG:
-            omv.disable_fb(True)
-    else:
-        sensor.set_pixformat(sensor.RGB565)
-        img = sensor.snapshot()
-        rgb_handler()
-        if OMV_DEBUG:
-            drawing_handler()
-            omv.disable_fb(False)
-    black_white = not black_white
+    init_sensor()
+    
+    load_tf_models()
+    init_colors()
+    
+    while(True):
+        clock.tick()
 
-    print(clock.fps(), "fps", end="\n\n")
+        if black_white:
+            sensor.set_pixformat(sensor.GRAYSCALE)
+            img = sensor.snapshot()
+            black_white_handler()
+            if OMV_DEBUG:
+                omv.disable_fb(True)
+        else:
+            sensor.set_pixformat(sensor.RGB565)
+            img = sensor.snapshot()
+            rgb_handler()
+            if OMV_DEBUG:
+                drawing_handler()
+                omv.disable_fb(False)
+        black_white = not black_white
+
+        print(clock.fps(), "fps", end="\n\n")
